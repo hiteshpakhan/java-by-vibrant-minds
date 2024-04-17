414db31e7ac78ba04a82eb653053616a6fd72900
hiteshpakhan <hitesh5pakhan@gmail.com>
2024-04-17 15:07:57 +0530

    a

--- program2.java
+++ program2.java
@@ -32,8 +32,83 @@
 
 
 
+        // int n=5;
+        // for(int i=n; i>=1; i--){
+        //     for(int j = i; j>=1; j--){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+        // 54321
+        // 4321
+        // 321
+        // 21
+        // 1
+
+
+        
+        // int n=5;
+        // for(int i = 1; i<=n; i++){
+        //     for(int j = i; j<=n; j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+        // 12345
+        // 2345
+        // 345
+        // 45
+        // 5
+
+
+
+
+        // int n=5;
+        // for(int i=1; i<=n; i++){
+        //     for(int j = n; j>=i; j--){
+        //         System.out.print((char)(j+96));
+        //     }
+        //     System.out.println();
+        // }
+        // edcba
+        // edcb
+        // edc
+        // ed
+        // e
+
+
+
+        // int n=5;
+        // for(int i = n; i>=1; i--){
+        //     for(int j=i; j>=1; j--){
+        //         System.out.print((char)(i+64));
+        //     }
+        //     System.out.println();
+        // }
+        // EEEEE
+        // DDDD
+        // CCC
+        // BB
+        // A
+
+
+
+        // int n=5;
+        // for(int i=n; i>=1; i--){
+        //     for(int j=i; j<=n; j++){
+        //         System.out.print((char)(j+64));
+        //     }
+        //     System.out.println();
+        // }
+        // E
+        // DE
+        // CDE
+        // BCDE
+        // ABCDE
+
+
+
 public class program2 {
     public static void main(String[] args) {
-        
     }
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+to compile this programe use the command 
+        javac filename.java
+
+to run the compiled files use the following command
+        java filename.java
