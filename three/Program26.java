c7117283b07ed780618df80f1f1efb75f23bbc55
hiteshpakhan <hitesh5pakhan@gmail.com>
2024-06-29 16:27:22 +0530

    a

--- three/Program26.java
+++ three/Program26.java
@@ -4,13 +4,16 @@ JDBC API-
     JDBC API stands for java database connectiuvity 
     by using jdbc api we can perform cruds operation 
         
-types of drivers : you can learn it from youtube
+	-types of drivers : you can learn it from youtube
 
-step 1: load a driver
-    to load a driver we can use forName() static method of Class class
-    forName() throws with ClasssNotFoudException hence to invoked this method we have to handle this exception 
-    Note: as per database driver can be varies
-    eg: 
+
+steps to connect with database
+----------------------------------------------
+step 1: load a driver		-(here we have to load a driver now we are using my sql driver so in practical we have load the mysql driver for that we can simple copy the dependency of that driver and past it into our dependences section into our xml file of our project it will automatically load our dependencies)
+    -to load a driver we can use forName() static method of Class class		-(note class.forNAme() we use this for to load external class file)
+    -forName() throws with ClasssNotFoudException hence to invoked this method we have to handle this exception 
+    -Note: as per database driver can be varies
+    -eg: 
         try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("driver loaded successfully");
@@ -19,7 +22,7 @@ step 1: load a driver
 			e.printStackTrace();
 		}
     
-step 2: create connection
+step 2: create connection	-(before this we have created the database name jap67 into our sql database and we will connect to this database here)
     to create a connection we can use getConnection() method of DriverMAnager class
     getConnection() method can be used to create a logical connection interface
     getConnection() method requires 3 parameter {url, username, password}
@@ -40,8 +43,58 @@ step 2: create connection
 			e.printStackTrace();
 		}
 
-step 3:
+step 3: create Query	-(before this we have create the table named t1 into our database jap67)
+	to create a query we can use string object
+	eg:
+	String sql = "insert into t1(name, city) values(?,?)";
+
+step4: create Statement
+	to create a statement we can use the createStatement() or prepare Statement() method of connection interface
+	createStatement() method returns instance of statement interface
+	prepareStatement() method returns instance of PreparedStatement interface
+	PreparedStatement interface is a child interface of Statement interface 
+	preparedStatement is faster than Statement because it does not create object of redundence query
+	eg:
+		Connection con = null;
+    	PreparedStatement pst = null;
+        try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			try {
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jap67", "root", "Luci@714");
+				String sql = "insert into t1(name, city) values(?,?)";
+				pst = con.prepareStatement(sql);
+				pst.setString(1, "def");
+				pst.setString(2, "mumbai");
+			} catch (SQLException e) {
+			..
+			}
+		}
 
 
+Step 5 : Execute Statement
+	to execute a statyement, we can use executeUpdate() or executeQuery() method of statement interface
+	to executeUpdate() method can be used if there is DML (insert, update, delete) query
+	executeQuery() method can be used if there is DQL (select) query	
+	executeUpdate() method returns int value
+	executeQuery() method returns instance of ResultSet interface
+	eg:
+		int check = pst.executeUpdate();
+	
+Step 6 : close Connection:
+	- to close a connection we can use finnally block
+ 	- connection can be closed in reverse order
+	eg: 
+	finally {
+				try {
+					pst.close();
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+	
 
  */
+
